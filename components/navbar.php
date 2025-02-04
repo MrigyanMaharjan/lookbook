317da3f8d143eec9c4c166241aa216679a504df6
Mrigyan <mrigyanmaharjan@gmail.com>
2025-02-04 22:38:49 +0545

    homepage

---
+++ components/navbar.php
@@ -0,0 +1,17 @@
+
+<html lang="en">
+<body>
+<nav>
+    <img class="logo" src="./assets/output-removebg-preview.png" alt="">
+    <section class="navigator"> 
+        <a class="nav-btn" href="#landing">HOME</a>
+        <a class="nav-btn" href="#about-us">ABOUT US</a>
+        <a class="nav-btn" href="#feature">FEATURES</a>
+    </section>
+    <section style="display:block; gap:20px" class="nav-signin"> 
+        <a class="nav-btn" href="./pages/login.php">LOG IN</a>
+        <a class="sigup-btn" href="./pages/sigin.php">SIGN UP</a>
+    </section>
+</nav>    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="./assets/output.jpg"> 
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOOKBOOK</title>
+    <title>SHARE YOUR STORY</title>
 </head>
 <body>
-    <a href="./pages/login.php">Log in</a>
-    <a href="./pages/sigin.php">Sign in</a>
+    <?php include "./components/navbar.php"?>
+    <div id="landing" class="landing">
+    <section class="hero">
+    <div class="hero-content">
+        <h1>Share Your Story, Inspire the World</h1>
+        <p>Everyone has a story to tell. Whether it’s a moment of triumph, a lesson learned, or a journey of self-discovery—<strong>your words matter</strong></p>
+        
+       
+
+        <a href="./pages/login.php" class="share-btn">Start Writing <i class="fa-solid fa-arrow-right"></i></a>
+    </div>
+
+    </section>
+    <section>
+        <img  class="img" src="./assets/8e5cf638-0e15-4ca5-820a-d4286896957d.png" alt="">
+    </section>
+    </div>
+    
+ 
+
+<a href="#about-us"><i class="fa-solid fa-angle-down floating-btn"></i>
+</a>
+    <div id="about-us" class="about-us"> This is an about us page</div>
+    <div id="feature" class="feature">This is a feature page</div>
 </body>
 </html>
